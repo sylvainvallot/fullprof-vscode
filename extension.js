08d11c848202434793181a9ec89c92cd44c1ffe2
Sylvain <sylvain.vallot98@gmail.com>
2021-02-18 15:38:02 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -19,7 +19,7 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fullprof-vscode.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('fullprof-vscode.hello', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
